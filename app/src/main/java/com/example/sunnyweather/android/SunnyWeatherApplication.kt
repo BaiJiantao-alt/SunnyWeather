1ab90fd718732d8cb0ff70c0ed2c679ac3f76b3f
baijiantao <baijiantao@bytedance.com>
2020-11-08 21:19:37 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/example/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,20 @@
+package com.example.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+
+    companion object {
+        const val TOKEN = "BiYvCJ7MOFi81dYf"
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+
+}
---
+++ app/src/main/java/com/example/sunnyweather/test.kt
@@ -0,0 +1,9 @@
+package com.example.sunnyweather
+
+import android.os.SystemClock
+
+fun main() {
+    for (i in 1 until 100) {
+        println()
+    }
+}
